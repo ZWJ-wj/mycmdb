5e8ca25b5c1b17debd5ffde0f7faac52e2d739ea
ZWJ-wj <2756026071@qq.com>
2023-02-23 15:38:38 +0800

    django init commit

--- settings.py
+++
@@ -1,8 +0,0 @@
-# -*- coding:utf-8 -*-
-import os
-base_path = os.path.dirname(__file__)
-setting = {
-    "debug": True,
-    "static_path": os.path.join(base_path, "static"),
-    "template_path": os.path.join(base_path, "templates")
-}
